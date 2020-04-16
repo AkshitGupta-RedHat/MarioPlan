bc454bd703d7155bb835b836885c744ef26227b8
Akshit Gupta <akshitguptasmart@gmail.com>
2020-04-16 02:34:40 -0400

    5,6 Linking the  dashboard with projects and notifications

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Navbar from './components/layout/navbar'
+import Dashboard from './components/dashboard/dashboard'
 
 // React, Redux & Firebase
 // Thunk used in redux works really well for async code
@@ -11,8 +12,12 @@ function App() {
   return (
     <BrowserRouter>
     <div className="App">
-     <h1>MarioPlan</h1>
+     <Navbar />
+     <Switch>
+       <Route path='/' component={Dashboard} />
+     </Switch>
     </div>
+    {/* <Route to="/" component={Dashboard}/> */}
     </BrowserRouter>
   );
 }
