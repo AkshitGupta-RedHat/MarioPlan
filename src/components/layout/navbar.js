add48ca09f991eecf78b2a0d35606120d92fa915
Akshit Gupta <akshitguptasmart@gmail.com>
2020-04-25 21:36:16 -0400

    25 Optionally displaying the links as per user is log in or not

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -3,22 +3,24 @@ import { Link } from 'react-router-dom'
 import SignedInLinks from './signedInLinks'
 import SignedOutLinks from './signedOutLinks'
 import {connect } from 'react-redux'
-const Navbar = () => {
+
+const Navbar = (props) => {
+    const isUserLogIn =props.logInId;
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo">MarioPlan</Link>
-                <SignedInLinks />
-                <SignedOutLinks />
+                {isUserLogIn? <SignedInLinks />:<SignedOutLinks />}  
             </div>
         </nav>
 
     )
 }
 const mapStateToProps = (state)=>{
-    console.log(state)
-return{
-
-}
+    console.log(state);
+    console.log(state.firebase.auth.uid)
+        return{
+            logInId:state.firebase.auth.uid
+        }
 }
 export default connect(mapStateToProps)(Navbar)
